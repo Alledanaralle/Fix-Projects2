6d3c841f84bc0805ca06ab3cff3c1c0e1324175b
Alledanaralle <139613761+Alledanaralle@users.noreply.github.com>
2024-05-25 19:54:36 +0700

    Add files via upload

---
+++ Dropdown/script.js
@@ -0,0 +1,10 @@
+function dropDown(){
+    document.querySelector('.menu-list').classList.toggle('active')
+    document.querySelector('.arrow').classList.toggle('active')
+    document.querySelector('.menu').classList.toggle('active')
+}
+function Drinks(){
+    document.querySelector('.drink-list').classList.toggle('active')
+    document.querySelector('.arrow-drink').classList.toggle('active')
+    document.querySelector('.menu-drink').classList.toggle('active')
+}
